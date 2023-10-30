50d3455971e52bd70d9a5e6456aebc0832922389
Yury Bayda <yury@bayda.pro>
2023-10-30 10:22:07 -0700

    Update test_mars_rover.py

--- test_mars_rover.py
+++ test_mars_rover.py
@@ -4,7 +4,7 @@ from mars_rover import MarsRover, Location
 class TestMarsRover:
 
     def test_stays_in_initial_location(self):
-        initial_location = Location(x=0, y=0, orientation="N")
+        initial_location = Location(0, 0, "N")
         rover = MarsRover(initial_location)
         rover.move("")
 
